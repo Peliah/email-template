fcdff4e5dff53c7189e9d84d376149a4a2ce920e
Peliah <pelepoupa@gmail.com>
2025-04-04 16:23:04 +0100

    added templates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import OTP from "@/components/Otp";
-import SubscriptionMail from "@/components/SubscriptionMail";
+
 
 export default function Home() {
   return (
     <>
-      {/* <SubscriptionMail /> */}
-      {/* <ForgotPassword /> */}
-      <OTP />
+      
     </>
   );
 }
---
+++ src/components/anniversary2.tsx
@@ -0,0 +1,27 @@
+
+const Anniversary1 = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <span>To Regina Daniels</span>
+            </div>
+            <div>
+                <p>Happy Anniversary</p>
+            </div>
+            <div>
+                <p>Celebrating your love and journey, [Recipient's Name]! In honor of your anniversary, a donation has been made on your behalf to support [Project Name] at MKE. Your enduring bond now inspires hope and positive change for children who need it most.</p>
+            </div>
+            <div>
+            Best of luck,<br />
+            Donald Franks
+            </div>
+        </div>
+        <div>
+            <Image src="/images/anniversary2.svg" alt="anniversary2" />
+        </div>
+    </div>
+  )
+}
+
+export default Anniversary1
